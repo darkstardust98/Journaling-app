16321bdb38f77894bc47aca1413a73893fc67f78
darkstardust98 <165183152+darkstardust98@users.noreply.github.com>
2024-09-08 17:54:27 +0200

    ADDED styles.css and UPDATED main.rs

--- RustFrontEnd/src/main.rs
+++ RustFrontEnd/src/main.rs
@@ -9,5 +9,24 @@ fn app() -> Html {
     let state = use_state(|| Model {
         value: 0
     });
+
+    let onclick = {
+        let state = state.clone();
+
+        Callback::from(move |_| {
+            state.set(Model {
+                value: state.value + 1
+            })
+        }
+    }
+
+    html! {
+        <div>
+            <button {onclick}>{ "+1" }</button>
+            <p>{ state.value }</p>
+        </div>
+    }
 }
-fn main() {}
+fn main() {
+    yew::start_app::<App>();
+}
